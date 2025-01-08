5212dd2d8dd0dbb6e4fe10f4fe0330406e5f04f8
Carlover21 <82425897+Carlover21@users.noreply.github.com>
2025-01-08 14:06:38 +0000

    Re created MainActivity.java and all the content to the buttons and the view

---
+++ app/src/main/java/com/rh/rentals/MainActivity.java
@@ -0,0 +1,35 @@
+package com.rh.rentals;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);  // Sets the layout for the home screen
+
+        // Set up click listener for the "View Cars" button
+        findViewById(R.id.btnViewCars).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start CarListActivity when the button is clicked
+                Intent intent = new Intent(MainActivity.this, CarListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set up click listener for the "Book a Car" button
+        findViewById(R.id.btnBookCar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start BookingActivity when the button is clicked
+                Intent intent = new Intent(MainActivity.this, BookingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
